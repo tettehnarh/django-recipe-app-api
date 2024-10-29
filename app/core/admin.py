5293b3fd8085c2a5f9595de985eb4440c34a3922
tettehnarh <leslienarh@outlook.com>
2024-10-29 15:18:08 +0000

    added user model and setup django admin

--- app/core/admin.py
+++ app/core/admin.py
@@ -26,7 +26,15 @@ class UserAdmin(BaseUserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'name', 'is_active', 'is_staff', 'is_superuser'),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser'
+            ),
         }),
     )
 
