0fcd1f9811a6a63ead8bc326d550ea51678d867d
tettehnarh <leslienarh@outlook.com>
2024-10-31 15:23:18 +0000

    added user and recipe API

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,8 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'user',
+    'recipe',
     'rest_framework',
+    'rest_framework.authtoken',
     'drf_spectacular',
+
 ]
 
 MIDDLEWARE = [
--- app/core/tests/tests_models.py
+++ app/core/tests/tests_models.py
@@ -1,7 +1,14 @@
+"""
+Tests for models
+"""
+# Django's Decimal data type
+from decimal import Decimal
 # Django's base test class for database-related tests
 from django.test import TestCase
 # Retrieves the currently active user model
 from django.contrib.auth import get_user_model
+# Import the models from the core app
+from core import models
 
 
 class ModelTests(TestCase):
@@ -53,3 +60,19 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_create_recipe(self):
+        """Test creating a recipe is successful"""
+        user = get_user_model().objects.create_user(
+            'test@example.com',
+            'testpass123'
+        )
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title='Sample recipe name',
+            time_minutes=5,
+            price=Decimal('5.50'),
+            description='Sample recipe description'
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
